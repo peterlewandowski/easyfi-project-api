9bd1397c61fa445a0deef4a1a3c4149c16ff2a4c
Peter Lewandowski <mapiotr2010@gmail.com>
2022-03-08 10:52:49 -0500

    framed out API

---
+++ src/strategies.js
@@ -0,0 +1,11 @@
+/* exports.createStrategy = (request, response) => {
+
+} */
+
+exports.getStrategies = (request, response) => {
+    response.send('Get Strategies is working')
+}
+
+/* exports.updateStrategy = (request,response) => {
+
+} */
