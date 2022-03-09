1ddc63f1eb3be8f0ded843ec293008eb79083e61
Peter Lewandowski <mapiotr2010@gmail.com>
2022-03-08 23:12:31 -0500

    added patch route to update enabled property

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { getStrategies, createStrategy } = require('./src/strategies');
+const { getStrategies, createStrategy, updateStrategy } = require('./src/strategies');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -10,7 +10,7 @@ app.use(express.json())
 // Routes
 app.post('/strategies', createStrategy)
 app.get('/strategies', getStrategies)
-// app.patch('/strategies/:strategyId', updateStrategy)
+app.patch('/strategies/:strategyId', updateStrategy)
 
 
 
--- src/strategies.js
+++ src/strategies.js
@@ -5,6 +5,7 @@ const { connectDb } = require('./connectDb')
 exports.createStrategy = (request, response) => {
     const newStrategy = {
         strategy: request.body.strategy,
+        enabled: true,
         created_at: FieldValue.serverTimestamp()
     }
     const db = connectDb();
@@ -27,6 +28,13 @@ exports.getStrategies = (request, response) => {
         .catch(err => response.status(500).send(err))
 }
 
-/* exports.updateStrategy = (request,response) => {
-
-} */
+exports.updateStrategy = (request,response) => {
+    const { strategyId } = request.params
+    const isEnabled = request.body.enabled
+    const db = connectDb();
+    db.collection("strategies")
+      .doc(strategyId)
+      .update({enabled: isEnabled}) // this will update the 'status' of a strategy (moon symbol)
+      .then((doc) => response.status(202).send(doc))
+      .catch((err) => response.status(500).send(err));    
+}
