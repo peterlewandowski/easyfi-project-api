f2e6e0bfc07b7b02bd1add120e03d4305949aee0
Peter Lewandowski <mapiotr2010@gmail.com>
2023-05-22 22:44:47 -0400

    back to old way to timestamp

--- src/strategies.js
+++ src/strategies.js
@@ -1,13 +1,13 @@
-const { Timestamp } = require("firebase-admin/firestore");
+const { FieldValue } = require("firebase-admin/firestore");
 const { connectDb } = require("./connectDb");
 
 exports.createStrategy = (request, response) => {
-  const db = connectDb();
   const newStrategy = {
     strategy: request.body,
-    createdAt: Timestamp.now(),
-    timestamp: Timestamp.now().seconds,
+    enabled: true,
+    created_at: FieldValue.serverTimestamp(),
   };
+  const db = connectDb();
   db.collection("strategies")
     .add(newStrategy)
     .then((doc) => response.status(201).send(doc.id))
@@ -29,30 +29,5 @@ exports.getStrategies = (request, response) => {
       });
       response.send(strategyList);
     })
-    .catch((err) => response.status(500).send(err));
-};
-
-exports.getOneStrategy = (request, response) => {
-  const { docId } = request.params;
-  const db = connectDb();
-  db.collection("strategies")
-    .doc(docId)
-    .get()
-    .then((doc) => response.status(200).send(doc.data()))
-    .catch((err) => response.status(500).send(err));
-};
-
-exports.updateStrategy = (request, response) => {
-  const { docId } = request.params;
-  const db = connectDb();
-  db.collection("strategies")
-    .doc(docId)
-    .update({
-      // strategy: request.body,
-      "strategy.enabled": request.body.enabled,
-      // "enabled": request.body.enabled,
-      created_at: FieldValue.serverTimestamp(),
-    })
-    .then(() => response.status(200).send("Record Updated"))
     .catch((err) => response.status(500).send(err));
 };
